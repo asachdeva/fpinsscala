883c57c9fa28cb6e0ecb6dd27b7ef37366ea9b6c
Akshay Sachdeva <asachdeva@citrine.io>
2020-02-25 15:13:33 -0600

    interim checkin

--- core/src/main/scala/fpinscala/datastructures/List.scala
+++ core/src/main/scala/fpinscala/datastructures/List.scala
@@ -3,6 +3,10 @@ package fpinscala.datastructures
 // adding sealed in front of trait means
 // all implementations of the trait must be defined
 // in the file
+// THe + in front of A indicates that A is a covariant or
+// positive parameter of List  So List[Dog] is considered
+// a subtype of List[Animal] assuming Dog is a subtype of
+// Animal
 sealed trait List[+A]
 case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
@@ -25,14 +29,16 @@ object List {
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
+
   // Ex 3.1
-  val x = List(1, 2, 3, 4, 5) match {
-    case Cons(x, Cons(2, Cons(4, _)))          => x
-    case Nil                                   => 42
-    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case Cons(h, t)                            => h + sum(t)
-    case _                                     => 101
+  val x = List(1,2,3,4,5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4,_)))) => x + y
+    case Cons(h,t) => h + sum(t)
+    case _ => 101
   }
+
 
   // Ex 3.2
   def tail[A](as: List[A]): List[A] =
@@ -60,58 +66,56 @@ object List {
   // Ex 3.5
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
     l match {
-      case Cons(h, t) if f(h) => dropWhile(t, f)
-      case _                  => l
+      case Cons(h,t) if f(h) => dropWhile(t,f)
+      case _ => l
     }
+
 
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
-      case Nil        => a2
-      case Cons(h, t) => Cons(h, append(t, a2))
+      case Nil => a2
+      case Cons(h,t) => Cons(h, append(t,a2))
     }
 
   def init[A](l: List[A]): List[A] =
     l match {
-      case Nil        => sys.error("init on an empty list")
-      case Cons(h, t) => Cons(h, init(t))
+      case Nil => sys.error("init on an empty list")
+      case Cons(h,t) => Cons(h,init(t))
     }
 
-  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+  def foldRight[A,B](as: List[A], z: B)(f: (A,B) => B): B =
     as match {
-      case Nil        => z
-      case Cons(h, t) => f(h, foldRight(t, z)(f))
+      case Nil => z
+      case Cons(h,t) => f(h,foldRight(t,z)(f))
     }
 
   def sum2(as: List[Int]): Int =
-    foldRight(as, 0)((x, y) => x + y)
+    foldRight(as, 0)((x,y) => x + y)
 
   def product2(ds: List[Double]): Double =
     foldRight(ds, 1.0)(_ * _)
 
   // Ex 3.9
   def length[A](as: List[A]): Int =
-    foldRight(as, 0)((_, acc) => acc + 1)
+    foldRight(as, 0)((_ , acc) => acc + 1)
 
-  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B =
+  def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B =
     as match {
-      case Nil        => z
-      case Cons(h, t) => foldLeft(t, f(z, h))(f)
+      case Nil => z
+      case Cons(h,t) => foldLeft(t,f(z,h))(f)
     }
 
   // Ex 3.11
   def sum3(as: List[Int]): Int =
     foldLeft(as, 0)(_ + _)
-
-  def product3(as: List[Double]): Double =
+  def product3(as:List[Double]): Double =
     foldLeft(as, 1.0)(_ * _)
-
   def length2[A](as: List[A]): Int =
-    foldLeft(as, 0)((acc, _) => acc + 1)
+    foldLeft(as, 0)((acc,_) => acc + 1)
 
   // Ex 3.12
   def reverse[A](as: List[A]): List[A] =
-    foldLeft(as, List[A]())((acc, h) => Cons(h, acc))
+    foldLeft(as, List[A]())((acc,h) => Cons(h,acc))
 
   // Ex 3.13
-
 }
