769bb323314b5794bd694e3a6a151e4f52c34d26
Akshay Sachdeva <asachdeva@citrine.io>
2020-04-01 13:38:18 -0500

    fix compile time issues

---
+++ core/src/main/scala/fpinscala/laziness/Stream.scala
@@ -0,0 +1,128 @@
+package fpinscala.laziness
+
+import Stream._
+
+// Laziness == Non Striictness
+// To say a function is non strict just means that the function
+// may not choose to evaluate one of its arguments.  In contrast a strict
+// function always evaluates its arguments
+//
+// Examples of standard non strict functions && || and the if control
+//
+// LazyList == Stream
+
+trait Stream[+A] {
+  // Ex 5.1
+
+  // The below function can stackoverflow
+  def toListRecursive: List[A] = this match {
+    case Cons(h, t) => h() :: t().toListRecursive
+    case _          => List()
+  }
+
+  def toList: List[A] = {
+    def go(s: Stream[A], acc: List[A]): List[A] = s match {
+      case Cons(h, t) => go(t(), h() :: acc)
+      case _          => acc
+    }
+    go(this, List()).reverse
+  }
+
+  def toListFast: List[A] = {
+    val buf = new scala.collection.mutable.ListBuffer[A]
+    def go(s: Stream[A]): List[A] = s match {
+      case Cons(h, t) =>
+        buf += h()
+        go(t())
+      case _ => buf.toList
+    }
+    go(this)
+  }
+
+  // Ex 5.2
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 1  => cons(h(), t().take(n - 1))
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+    case _                    => this
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
+    case _ => this
+  }
+
+  // Ex 5.3
+  def takeWhile(f: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if (f(h())) => cons(h(), t() takeWhile f)
+    case _ => empty
+  }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
+    this match {
+      case Cons(h, t) => f(h(), t().foldRight(z)(f))
+      case _ => z
+    }
+
+  def exists(f: A => Boolean): Boolean =
+    foldRight(false)((a, b) => f(a) || b)
+
+  // Ex 5.4
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(true)((a, b) => p(a) && b)
+
+  // Ex 5.5
+  def takWhile_1(f: A => Boolean): Stream[A] =
+    foldRight(empty[A])((h, t) => if (f(h)) cons(h, t) else t)
+
+  // Ex 5.6
+  def headOption: Option[A] =
+    foldRight(None: Option[A])((h, _) => Some(h))
+
+  // Ex 5.7
+  def map[B](f: A => B): Stream[B] =
+    foldRight(empty[B])((h, t) => cons(f(h), t))
+
+  def filter(f: A => Boolean): Stream[A] =
+    foldRight(empty[A])((h, t) => if (f(h)) cons(h, t) else t)
+
+   def append[B>:A](s: => Stream[B]): Stream[B] =
+    foldRight(s)((h,t) => cons(h,t))
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] =
+    foldRight(empty[B])((h,t) => f(h) append t)
+
+
+}
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  // smart constructor
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+
+  // Ex 5.8
+  def constant[A](a: A): Stream[A] = {
+    lazy val tail: Stream[A] = Cons(() => a, () => tail)
+    tail
+  }
+
+  // Ex 5.9
+  def from(n: Int): Stream[Int] =
+    cons(n, from(n+1))
+
+  // Ex 5.10
+  val fibs = {
+    def go(f0: Int, f1: Int): Stream[Int] =
+      cons(f0, go(f1, f0 + f1))
+    go(0, 1)
+  }
+
+}
