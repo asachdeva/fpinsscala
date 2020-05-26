722e19b170621bbcf215db9bb35d07d38796a0dc
Akshay Sachdeva <asachdeva@citrine.io>
2020-05-26 12:33:30 -0500

    added Tree tests

--- core/src/test/scala/com/fpinscala/datastructures/TreeSuite.scala
+++ core/src/test/scala/com/fpinscala/datastructures/TreeSuite.scala
@@ -1,5 +1,20 @@
 package com.fpinscala.datastructures
 
 class TreeSuite extends munit.FunSuite {
-}
+  test("Tree Tests") {
+    import fpinscala.datastructures.Tree._
+    val t1 = branch(branch(branch(leaf(1), leaf(2)), leaf(3)), branch(leaf(5), leaf(6)))
+    val t2 = branch(branch(branch(leaf(2), leaf(3)), leaf(4)), branch(leaf(6), leaf(7)))
+    assert(size(t1) == 9)
+    assert(sizeViaFold(t1) == 9)
 
+    assert(maximum(t1) == 6)
+    assert(maximumViaFold(t1) == 6)
+
+    assert(depth(t1) == 3)
+    assert(depthViaFold(t1) == 3)
+
+    assert(map(t1)(_ + 1) == t2)
+    assert(mapViaFold(t1)(_ + 1) == t2)
+  }
+}
