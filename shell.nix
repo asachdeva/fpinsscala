f2a70f26b06c9de0c2c1976a4d7c340e634ea79b
Akshay Sachdeva <asachdeva@alumni.utexas.net>
2021-04-06 23:06:24 -0500

    Create shell.nix

---
+++ shell.nix
@@ -0,0 +1,34 @@
+{ jdk ? "jdk15" }:
+
+let
+  java = pkgs.${jdk};
+
+  config = {
+    packageOverrides = pkgs: rec {
+      sbt = pkgs.sbt.overrideAttrs (
+        old: rec {
+          patchPhase = ''
+            echo -java-home ${java} >> conf/sbtopts
+          '';
+        }
+      );
+    };
+  };
+
+  nixpkgs = fetchTarball {
+    name   = "nixos-unstable-2021-02-21";
+    url    = "https://github.com/NixOS/nixpkgs/archive/9816b99e71c.tar.gz";
+    sha256 = "1dpz36i3vx0c1wmacrki0wsf30if8xq3bnj71g89rsbxyi87lhcm";
+  };
+
+  pkgs = import nixpkgs { inherit config; };
+in
+pkgs.mkShell {
+  name = "scala-shell";
+
+  buildInputs = with pkgs; [
+    gnupg
+    java
+    sbt
+  ];
+}
